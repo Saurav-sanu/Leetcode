204786a7e25cdc5da5ad6fb112baff471ce863f2
Saurav-sanu <142467513+Saurav-sanu@users.noreply.github.com>
2025-02-17 11:08:21 +0530

    Time: 0 ms (100%), Space: 8.2 MB (50.35%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-int t[101][101];
-bool solve(int i,int open,string &s,int n){
-    //base case
-    bool is_valid=false;
-    if(i==n){
-        return open==0;
-    }
-    if(t[i][open]!=-1){
-        return t[i][open];
-    }
-
-
-    if(s[i]=='('){
-        is_valid|=solve(i+1,open+1,s,n);
-    }
-    else if(s[i]=='*'){
-        is_valid|=solve(i+1,open+1,s,n);
-        is_valid|=solve(i+1,open,s,n);
-        if(open>0){
-            is_valid|=solve(i+1,open-1,s,n);
-        }
-    }
-    else if(open>0){
-        is_valid|=solve(i+1,open-1,s,n);
-    }
-    return  t[i][open]=is_valid;
-
-}
     bool checkValidString(string s) {
-        //option 1
-        // recursion se
-        memset(t,-1,sizeof(t));
-        int n=s.length();
-        return solve(0,0,s,n);
+        stack<int>st;
+        stack<int>starst;
+
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else if(s[i]==')'){
+                if(!st.empty()){
+                    st.pop();
+                }
+                else if(!starst.empty()){
+                    starst.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                starst.push(i);
+            }
+        }
+
+        //post prosssesing
+        while(!st.empty() && !starst.empty()){
+            if(st.top()>starst.top()){
+                return false;
+            }
+            st.pop();
+            starst.pop();
+        }
+        return st.empty();
     }
 };
