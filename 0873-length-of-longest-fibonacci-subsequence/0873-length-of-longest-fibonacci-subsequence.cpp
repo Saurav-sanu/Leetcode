c18265aaee52e1ed8bed54522d123e5fefdb782a
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-02-27 23:10:01 +0530

    Time: 343 ms (40.17%), Space: 150 MB (21.37%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,72 +1,28 @@
 class Solution {
 public:
-int solve(int j,int k ,vector<int>&arr,unordered_map<int,int>&mp){
-    int target=arr[k]-arr[j];
-
-    if(mp.find(target)!=mp.end() && mp[target]<j){
-        int i=mp[target];
-        return solve(i,j,arr,mp)+1;
-    }
-    return 2;
-}
+// /using boottom up 
     int lenLongestFibSubseq(vector<int>& arr) {
         int n=arr.size();
+        int max_length=0;
+        vector<vector<int>>t(n,vector<int>(n,2));
         unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
             mp[arr[i]]=i;
         }
-        int max_length=0;
+
         for(int j=1;j<n;j++){
             for(int k=j+1;k<n;k++){
-                int length=solve(j,k,arr,mp);
-                if(length>=3){
-                    max_length=max(length,max_length);
+                int target=arr[k]-arr[j];
+                if(mp.count(target) && mp[target]<j){
+                    int i=mp[target];
+                    t[j][k]=t[i][j]+1;
                 }
+                max_length=max(max_length,t[j][k]);
             }
         }
-        return max_length;
-    }
-};
 
-here time is 0(n^3)
-space is 0(n)//recursion ka stack use ho rha hia isliye.
+        return max_length>=3?max_length:0;
 
-
-
-//using memoisation 
-time is o(n^2)
-space is 0(n) recusion stack ka space hai woh 
-
-class Solution {
-public:
-int solve(int j,int k ,vector<int>&arr,unordered_map<int,int>&mp,vector<vector<int>>&t){
-    if(t[j][k]!=-1){
-        return t[j][k];
-    }
-    int target=arr[k]-arr[j];
-
-    if(mp.find(target)!=mp.end() && mp[target]<j){
-        int i=mp[target];
-        return t[j][k]=solve(i,j,arr,mp,t)+1;
-    }
-    return t[j][k]=2;
-}
-    int lenLongestFibSubseq(vector<int>& arr) {
-        int n=arr.size();
-        unordered_map<int,int>mp;
-        vector<vector<int>>t(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            mp[arr[i]]=i;
-        }
-        int max_length=0;
-        for(int j=1;j<n;j++){
-            for(int k=j+1;k<n;k++){
-                int length=solve(j,k,arr,mp,t);
-                if(length>=3){
-                    max_length=max(length,max_length);
-                }
-            }
-        }
-        return max_length;
+        
     }
 };
