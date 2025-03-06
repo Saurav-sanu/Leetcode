64632cae7cf325fcca660c0223801e1a0576eb3b
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-03-06 19:23:23 +0530

    Time: 0 ms (100%), Space: 18.8 MB (26.02%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
+bool hasCycle=false;
+    void DFS(unordered_map<int,vector<int>>&adj,vector<bool>&inRecursion,int u,vector<bool>&visited,stack<int>&st){
+        visited[u]=true;
+        inRecursion[u]=true;
+
+        for(int &v:adj[u]){
+            if(inRecursion[v]==true){
+                hasCycle=true;
+                return ;
+            }
+
+            if(!visited[v]){
+                DFS(adj,inRecursion,v,visited,st);
+            }
+                
+        }
+        st.push(u);
+        inRecursion[u]=false;
+
+    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        //ager cycle hua toh false wrna 
-        // kahn' algo
-
-        vector<int>result;
-        int count=0;
-        queue<int>q;
-        vector<int>indegree(numCourses,0);
-
         unordered_map<int,vector<int>>adj;
+        vector<bool>inRecursion(numCourses,false);
+        vector<bool>visited(numCourses,false);
+        stack<int>st;
+
         for(auto &vec:prerequisites){
             int u=vec[0];
             int v=vec[1];
 
             adj[v].push_back(u);
-            indegree[u]++;
         }
 
-        //step 2
+        vector<int>result;
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-                count++;
-                result.push_back(i);
+            if(!visited[i]){
+                DFS(adj,inRecursion,i,visited,st);
             }
         }
 
-        //step 3
-        while(!q.empty()){
-            int top=q.front();
-            q.pop();
-
-            for(int &v:adj[top]){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    count++;
-                    result.push_back(v);
-                    q.push(v);
-                }
-            }
-        }
-
-        if(count==numCourses){
-            return result;
-        }
-        else{
+        if(hasCycle==true){
             return {};
         }
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+
+
+        return result;
+
 
     }
 };
