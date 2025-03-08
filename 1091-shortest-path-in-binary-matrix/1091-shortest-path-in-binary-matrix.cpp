1782a570bfc9060da9454d2c95f44716aaed3b74
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-03-08 18:00:27 +0530

    Time: 281 ms (6.55%), Space: 83 MB (5.82%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+typedef pair<int,pair<int,int>>P;
+vector<vector<int>>direction={{1,1},{1,-1},{-1,1},{-1,-1},{0,1},{0,-1},{1,0},{-1,0}};
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
+        int m=grid.size();
+        int n=grid[0].size();
+        if(m==0 ||n==0 ||grid[0][0]!=0){
+            return -1;
+        }
+        auto isSafe=[&](int a,int b){
+            return (a>=0 && a<m && b>=0 &&b<n);
+        };
+
+
+        //using dijkastra algo
+        vector<vector<int>>result(m,vector<int>(n,INT_MAX));
+        priority_queue<P,vector<P>,greater<P>>q;
+        result[0][0]=0;
+        q.push({0,{0,0}});
+
+        while(!q.empty()){
+           int d=q.top().first;
+           auto &front=q.top().second;
+           int x=front.first;
+           int y=front.second;
+           q.pop();
+
+            for(auto dir:direction){
+                int x_dir=x+dir[0];
+                int y_dir=y+dir[1];
+                int dist=1;
+                if(isSafe(x_dir,y_dir) && grid[x_dir][y_dir]==0 && d+dist<result[x_dir][y_dir]){
+                    q.push({d+dist,{x_dir,y_dir}});
+                    result[x_dir][y_dir]=d+dist;
+                }
+            }
+
+
+
+        }
+        if(result[m-1][n-1]==INT_MAX){
+            return -1;
+        }
+        return result[m-1][n-1]+1;
+
+    }
+};
