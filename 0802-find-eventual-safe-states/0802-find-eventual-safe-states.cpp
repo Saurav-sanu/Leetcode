3f4cf1896e779e8d0f0c4478a3f94a0a5de25793
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-03-15 17:17:00 +0530

    Update 0802-find-eventual-safe-states.cpp

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,6 +1,54 @@
 class Solution {
+ public:
+ bool DFS(vector<bool>&visited,vector<bool>&inRecursion,int u,vector<vector<int>>& graph){
+     //using dfs
+     visited[u]=true;
+     inRecursion[u]=true;
+ 
+     for(auto v:graph[u]){
+         if(visited[v]==false && DFS(visited,inRecursion,v,graph)){
+             return true;
+         }
+         if(inRecursion[v]==true && visited[v]==true){
+             return true;
+         }
+ 
+     }
+ 
+     inRecursion[u]=false;
+     return false;
+ 
+ }
+     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+         //using dfs 
+         int n=graph.size();
+         vector<bool>inRecursion(n,false);
+         vector<bool>visited(n,false);
+ 
+ 
+         for(int i=0;i<n;i++){
+             if(!visited[i]){
+                 DFS(visited,inRecursion,i,graph);
+             }
+         }
+         vector<int >ans;
+ 
+         for(int i=0;i<inRecursion.size();i++){
+             if(inRecursion[i]==false){
+                 ans.push_back(i);
+             }
+         }
+         return ans;
+         
+     }
+ };
+
+//using bfs
+
+class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //using kahn algo and using bfs
         int n=graph.size();
         vector<vector<int>>adj(n);
         vector<int>inDegree(n,0);
@@ -44,8 +92,5 @@ public:
         }
 
         return safeNode;
-
-
-
     }
 };
