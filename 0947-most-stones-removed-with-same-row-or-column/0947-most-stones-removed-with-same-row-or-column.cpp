bc495db7e817120cbceb87bbd809c3d5605b0335
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-03-20 23:35:45 +0530

    Update 0947-most-stones-removed-with-same-row-or-column.cpp

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+//using DFS
+void DFS(vector<vector<int>>&stones,vector<bool>&visited,int u){
+    visited[u]=true;
+
+    for(int i=0;i<stones.size();i++){
+        int r=stones[u][0];
+        int c=stones[u][1];
+        if(!visited[i] && (stones[i][0]==r ||stones[i][1]==c)){
+            DFS(stones,visited,i);
+        }
+    }
+}
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        int group=0;
+        vector<bool>visited(n,false);
+        for(int i=0;i<n;i++){
+            if(visited[i]==true){
+                continue;
+            }
+            DFS(stones,visited,i);
+            group++;
+        }
+        return n-group;
+    }
+};
+
+//usign union and find
+class Solution {
+public:
 vector<int>parent;
 vector<int>rank;
 int find(int x){
