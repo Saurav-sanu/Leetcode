9d79c2b928d4c8c6ab9c393f940f2fab1bded60e
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-01 15:35:48 +0530

    Time: 0 ms (100%), Space: 10 MB (89.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+int t[101];
 int solve(vector<int>&nums,int index){
     //base case
     if(index>=nums.size()){
         return 0;
     }
+    if(t[index]!=-1){
+        return t[index];
+    }
 
     int include=solve(nums,index+2)+nums[index];
     int exclude=solve(nums,index+1);
 
-    return max(include,exclude);
+    return t[index]=max(include,exclude);
 }
     int rob(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
         return solve(nums,0);
     }
 };
