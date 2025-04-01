b9a53b0e18ded9de698c512bfd91cb580250bf09
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-01 21:13:43 +0530

    Update 0198-house-robber.cpp

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -25,3 +25,33 @@ public:
 
     }
 };
+
+
+//uisng space optimisatoin\
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       
+
+        //using space optimisation
+        int n=nums.size();
+        vector<int>t(n+1,0);
+
+        int prevprev=0;
+        int prev=nums[0];
+
+        for(int i=2;i<=n;i++){
+            int skip=prev;
+            int take=nums[i-1]+prevprev;
+
+            int temp=max(skip,take);
+
+            prevprev=prev;
+            prev=temp;
+        }
+
+        return prev;
+
+
+    }
+};
