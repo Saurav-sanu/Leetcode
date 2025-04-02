82c8e2e3267d89ef87b4c048115a1bd6df169704
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-03 02:48:48 +0530

    Update 1911-maximum-alternating-subsequence-sum.cpp

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -26,3 +26,25 @@ long long solve(int index,bool flag,vector<int>& nums,vector<vector<long long>>&
         return solve(0,true,nums,dp);
     }
 };
+
+//using bottom up approach
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        //bottom up
+        
+        int n=nums.size();
+        vector<vector<long long >>dp(n+1,vector<long long>(2,0));
+
+
+        for(int i=1;i<n+1;i++){
+            //even
+            dp[i][0]=max(dp[i-1][1]-nums[i-1],dp[i-1][0]);
+
+            //odd
+            dp[i][1]=max(dp[i-1][0]+nums[i-1],dp[i-1][1]);
+
+        }
+        return max(dp[n][0],dp[n][1]);
+    }
+};
