3465b721e5882f99cf7565d9b89a310e257dd013
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-04 04:34:32 +0530

    Update 0300-longest-increasing-subsequence.cpp

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,3 +1,31 @@
+using recuriosn and memoisation
+
+int solveLIS(int prevIndex,int index,vector<int>&nums,vector<vector<int>>&t){
+    //base case
+    if(index>=nums.size()){
+        return 0;
+    }
+
+    if(t[prevIndex+1][index]!=-1){
+        return t[prevIndex+1][index];
+    }
+    int take=0;
+    if(prevIndex==-1 || nums[prevIndex]<nums[index]){
+        take=1+solveLIS(index,index+1,nums,t);
+    }
+    int skip=solveLIS(prevIndex,index+1,nums,t);
+
+    if(prevIndex!=-1){
+
+        t[prevIndex+1][index]=max(skip,take);
+    }
+
+    return max(skip,take);
+
+}
+
+
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
