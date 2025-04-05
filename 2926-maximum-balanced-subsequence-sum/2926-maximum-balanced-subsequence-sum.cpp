6bccc99d49c40572e52d0209cb70c6b098ec0593
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-05 16:34:13 +0530

    Update 2926-maximum-balanced-subsequence-sum.cpp

--- 2926-maximum-balanced-subsequence-sum/2926-maximum-balanced-subsequence-sum.cpp
+++ 2926-maximum-balanced-subsequence-sum/2926-maximum-balanced-subsequence-sum.cpp
@@ -1,3 +1,63 @@
+//using rescutsion LIS
+class Solution {
+public:
+long long solve(int prev,int index,vector<int>&nums){
+    //base case
+    if(index>=nums.size()){
+        return 0;
+    }
+    int take=0;
+    if(prev==-1 ||nums[prev]-prev<=nums[index]-index){
+        take=nums[index]+solve(index,index+1,nums);
+
+    }
+    int skip=solve(prev,index+1,nums);
+    return max(take,skip);
+
+}
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
+        int maxElement=*max_element(nums.begin(),nums.end());
+        if(maxElement<=0){
+            return maxElement;
+        }
+        return solve(-1,0,nums);
+    }
+};
+
+//USING BOTTOM UP OF LIS
+class Solution {
+public:
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
+        int n=nums.size();
+
+        int maxElement=*max_element(nums.begin(),nums.end());
+
+        if(maxElement<=0){
+            return maxElement;
+        }
+
+        vector<long long>t(n);
+        for(int i=0;i<n;i++){
+            t[i]=nums[i];
+        }
+
+        long long maxSum=INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]-i>=nums[j]-j){
+                    t[i]=max<long long>(t[i],t[j]+nums[i]);
+                    maxSum=max(maxSum,t[i]);
+                }
+            }
+        }
+        return maxSum>maxElement?maxSum:maxElement;
+    }
+};
+
+
+
+
+
 class Solution {
 public:
     long long maxBalancedSubsequenceSum(vector<int>& nums) {
