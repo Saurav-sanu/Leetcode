2a719d074301df0543fcccfdb7141052ae63ee8e
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-05 17:13:05 +0530

    Time: 0 ms (100%), Space: 14.2 MB (78.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,49 +1,24 @@
-using recuriosn and memoisation
-
-int solveLIS(int prevIndex,int index,vector<int>&nums,vector<vector<int>>&t){
-    //base case
-    if(index>=nums.size()){
-        return 0;
-    }
-
-    if(t[prevIndex+1][index]!=-1){
-        return t[prevIndex+1][index];
-    }
-    int take=0;
-    if(prevIndex==-1 || nums[prevIndex]<nums[index]){
-        take=1+solveLIS(index,index+1,nums,t);
-    }
-    int skip=solveLIS(prevIndex,index+1,nums,t);
-
-    if(prevIndex!=-1){
-
-        t[prevIndex+1][index]=max(skip,take);
-    }
-
-    return max(skip,take);
-
-}
-
-
-
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        //using bootom up appracoh
+        //using patience sorting
         int n=nums.size();
-        vector<int>t(n,1);
+        vector<int>sorted;
 
-        int maxLis=1;
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    t[i]=max(t[i],t[j]+1);
-                    maxLis=max(maxLis,t[i]);
+            auto it=lower_bound(sorted.begin(),sorted.end(),nums[i]);
 
-                }
+            if(it==end(sorted)){
+                sorted.push_back(nums[i]);
+            }
+            //aasa element mil gy hai jo usse greater hai 
+            else{
+                *it=nums[i];
             }
         }
-        return maxLis;
+        return sorted.size();
+
 
     }
 };
