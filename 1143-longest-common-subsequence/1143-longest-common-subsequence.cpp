79e8d52027bf20dab055cfcda168eb9a8d6d3dda
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-11 22:38:04 +0530

    Time: 27 ms (55.69%), Space: 27.5 MB (50.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,38 +1,3 @@
-//using recursive and memoisation approach
-class Solution {
-public:
-int t[1001][1001];
-int solve(int index1,int index2,string &s1,string &s2){
-    //base case
-    if(index2>=s2.length()){
-        return 0;
-    }
-    if(index1>=s1.length()){
-        return 0;
-    }
-    if(t[index1][index2]!=-1){
-        return t[index1][index2];
-    }
-    //take
-    int take=0;
-    int nottake=0;
-    if(s1[index1]==s2[index2]){
-        take=1+solve(index1+1,index2+1,s1,s2);
-    }
-    else{
-        nottake=max(solve(index1,index2+1,s1,s2),solve(index1+1,index2,s1,s2));
-    }
-
-    return t[index1][index2]=take+nottake;
-}
-    int longestCommonSubsequence(string text1, string text2) {
-        memset(t,-1,sizeof(t));
-        return solve(0,0,text1,text2);
-    }
-};
-
-//using bottom up approach
-
 class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
