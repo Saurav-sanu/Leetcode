2e66ae70a48997a1f0ab59a1dd9fac2567a65f6a
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-15 03:09:45 +0530

    Time: 231 ms (23.36%), Space: 30 MB (32.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -17,45 +17,57 @@ bool solve(string &s,int i,int j){
 }
     string longestPalindrome(string s) {
         int n=s.length();
-        int maxLen=0;
-        int sp=0;
-        memset(t,-1,sizeof(t));
 
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(s,i,j)==true){
-                    if(j-i+1>maxLen){
-                        maxLen=j-i+1;
-                        sp=i;
-                    }
+        // int sp=0;
+        // memset(t,-1,sizeof(t));
 
-                }
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         if(solve(s,i,j)==true){
+        //             if(j-i+1>maxLen){
+        //                 maxLen=j-i+1;
+        //                 sp=i;
+        //             }
 
-        return s.substr(sp,maxLen);
-        // vector<vector<bool>>t(n,vector<bool>(n,false));
-        // string ans="";
-
-        // for(int l=1;l<=n;l++){
-        //     for(int i=0;i+l-1<n;i++){
-        //         int j=i+l-1;
-        //         if(i==j){
-        //             t[i][j]=true;
         //         }
-        //         else if(i+1==j){
-        //              t[i][j]=(s[i]==s[j]);
-        //         }
-        //         else{
-        //             t[i][j]=(s[i]==s[j] && t[i-1][j+1]);
-        //         }
-
-        //          if(t[i][j]==true){
-        //             count++;
-        //         }
-
         //     }
         // }
+
+        // return s.substr(sp,maxLen);
+
+
+        vector<vector<bool>>t(n,vector<bool>(n,false));
+        string ans="";
+        int maxLen=1;
+        int sp=0;
+
+        for(int l=1;l<=n;l++){
+            for(int i=0;i+l-1<n;i++){
+                int j=i+l-1;
+                if(i==j){
+                    t[i][j]=true;
+                }
+                else if(s[i]==s[j] && l==2){
+
+                    t[i][j]=true;
+                    maxLen=j-i+1;
+                    sp=i;
+                }
+                else if(s[i]==s[j] && t[i+1][j-1]){
+                   t[i][j]=true;
+                   if(j-i+1>maxLen){
+                    maxLen=j-i+1;
+                    sp=i;
+                   }
+                }
+                else{
+                    t[i][j]=false;
+                }
+
+
+            }
+        }
+        return s.substr(sp,maxLen);
         
     }
 };
