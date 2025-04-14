9734d11b162d9fda10aab2aedbc68687b689c5d0
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-14 15:09:52 +0530

    Time: 99 ms (5.74%), Space: 80.4 MB (12.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,45 +1,6 @@
 class Solution {
 public:
 int t[501][501];
-int solve(int i,int j,string a, string b){
-    //base case
-    if(i>=a.length()){
-        return b.length()-j;
-    }
-    if(j>=b.length()){
-        return a.length()-i;
-    }
-    if(t[i][j]!=-1){
-        return t[i][j];
-    }
-
-
-    int result=0;
-
-    if(a[i]!=b[j]){
-        int update=1+solve(i+1,j+1,a,b);
-        int delet=1+solve(i+1,j,a,b);
-        int insert=1+solve(i,j+1,a,b);
-        result=min({update,delet,insert});
-    }
-    else{
-        result=solve(i+1,j+1,a,b);
-        
-    }
-    return t[i][j]=result;
-
-}
-    int minDistance(string word1, string word2) {
-        memset(t,-1,sizeof(t));
-        return solve(0,0,word1,word2);
-    }
-};
-
-//another way of writing the same code
-//from right to left only
-class Solution {
-public:
-int t[501][501];
 int solve(int m,int n,string a, string b){
     //base case
     if(m==0 || n==0){
