28b8babc8228948719f0a6286e49cd3f1165df76
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-14 15:10:03 +0530

    Time: 9 ms (39.96%), Space: 13.1 MB (60.35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-int t[501][501];
-int solve(int m,int n,string a, string b){
-    //base case
-    if(m==0 || n==0){
-        return m+n;
-    }
+    int minDistance(string a, string b) {
+        int m=a.size();
+        int n=b.size();
+        vector<vector<int>>t(m+1,vector<int>(n+1));
 
-    if(t[m][n]!=-1){
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0){
+                    t[i][j]= i+j;
+                }
+                else if(a[i-1]==b[j-1]){
+                    t[i][j]=t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=1+min({t[i-1][j-1],t[i-1][j],t[i][j-1]});
+                }
+            }
+        }
         return t[m][n];
-    }
-
-
-    int result=0;
-
-    if(a[m-1]!=b[n-1]){
-        int update=1+solve(m-1,n-1,a,b);
-        int delet=1+solve(m-1,n,a,b);
-        int insert=1+solve(m,n-1,a,b);
-        result=min({update,delet,insert});
-    }
-    else{
-        result=solve(m-1,n-1,a,b);
-        
-    }
-    return t[m][n]=result;
-
-}
-    int minDistance(string word1, string word2) {
-        memset(t,-1,sizeof(t));
-        int m=word1.size();
-        int n=word2.size();
-        return solve(m,n,word1,word2);
     }
 };
