85309abb2d8e67625c68cc74453711d5be2cabca
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-15 03:07:33 +0530

    Time: 179 ms (27.86%), Space: 13.9 MB (58.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -3,7 +3,7 @@ public:
 int t[1001][1001];
 bool solve(string &s,int i,int j){
     if(i>=j){
-        return true;
+        return 1;
     }
     if(t[i][j]!=-1){
         return t[i][j];
@@ -13,7 +13,7 @@ bool solve(string &s,int i,int j){
         return t[i][j]=solve(s,i+1,j-1);
     }
 
-    return false;
+    return t[i][j]=0;
 }
     string longestPalindrome(string s) {
         int n=s.length();
