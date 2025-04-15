b6734b141bf814c0bc17be904c1d724048ff48f1
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-15 23:20:59 +0530

    Time: 4 ms (1.41%), Space: 8 MB (86.11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-int t[101][101];
-vector<vector<int>>direction={{1,0},{0,1}};
-int solve(int i,int j,int m,int n){
-    auto isSafe=[&](int a,int b){
-        return(a>=0 && a<m && b>=0&& b<n);
-    };
-
+int solve(int i,int j,int m ,int n,vector<vector<int>>&dp){
+    //base case
+    if(i<0 ||i>=m || j<0 ||j>=n){
+        return 0;
+    }
     if(i==m-1 && j==n-1){
         return 1;
     }
-    if(t[i][j]!=-1){
-        return t[i][j];
+    if(dp[i][j]!=-1){
+        return dp[i][j];
     }
-    int result=0;
-
-    for(auto dir:direction){
-        int new_i=i+dir[0];
-        int new_j=j+dir[1];
-
-
-        if(isSafe(new_i,new_j)){
-            result+=solve(new_i,new_j,m,n);
-        }
-    }
-    return t[i][j]=result;
+   
+    int right =solve(i+1,j,m,n,dp);
+    int down=solve(i,j+1,m,n,dp);
+    return dp[i][j]=right+down;
 }
     int uniquePaths(int m, int n) {
-        memset(t,-1,sizeof(t));
-
-        return solve(0,0,m,n);
+        int i=0;
+        int j=0;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(i,j,m,n,dp);
     }
 };
