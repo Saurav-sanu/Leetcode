057c2114252af52ca4fb3d1b105e942517d6361d
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-15 23:30:50 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -21,5 +21,28 @@ int solve(int i,int j,int m,int n){
         memset(t,-1,sizeof(t));
 
         return solve(0,0,m,n);
+
+
+         //using bottom up approach
+        vector<vector<int>>t(m+1,vector<int>(n+1));
+        t[0][0]=0;
+        for(int i=0;i<m;i++){
+            //row filling
+
+            t[i][0]=1;
+        }
+        for(int i=0;i<n;i++){
+            //coulmn filling 
+            t[0][i]=1;
+        }
+
+        for(int i=2;i<m;i++){
+            for(int j=2;j<n;j++){
+                t[i][j]=t[i-1][j]+t[i][j-1];
+            }
+        }
+
+        return t[m-1][n-1];
+
     }
 };
