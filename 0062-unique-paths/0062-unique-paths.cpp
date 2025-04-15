4a5d0455801d849167081c263bb03aca71fcf7e5
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-15 23:35:53 +0530

    Time: 3 ms (8.36%), Space: 9.4 MB (8.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
 
     int uniquePaths(int m, int n) {
-        if(m==1 && n==1){
-            return 1;
-        }
+       
          //using bottom up approach
         vector<vector<int>>t(m,vector<int>(n));
-        t[0][0]=0;
-        for(int i=1;i<m;i++){
+        
+        for(int i=0;i<m;i++){
             //row filling
             t[i][0]=1;
         }
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             //column filling 
             t[0][i]=1;
         }
