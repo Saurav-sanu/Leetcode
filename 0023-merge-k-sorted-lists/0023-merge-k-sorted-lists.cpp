a8e0f9c677c4e36932ea981ad3ecf1f2ca6f4dce
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-29 14:55:46 +0530

    Update 0023-merge-k-sorted-lists.cpp

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -17,8 +17,7 @@ ListNode* mergetwolist(ListNode* list1,ListNode*list2){
     if(list2==NULL){
         return list1;
     }
-
-
+    
     if(list1->val<=list2->val){
         list1->next=mergetwolist(list1->next,list2);
         return list1;
