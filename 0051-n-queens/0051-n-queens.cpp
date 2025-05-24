ce8f1ad62361cb674cceb5408af6622e791c7e65
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-05-25 01:20:52 +0530

    Time: 10 ms (19.1%), Space: 12 MB (23.98%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
+//appraoch 2
+unordered_set<int>cols;
+unordered_set<int>diagonals;
+unordered_set<int>antidiagonals;
 
-bool isvalid(int n,vector<string>&board,int row,int col){
-    //
-    for(int i=row-1;i>=0;i--){
-        if(board[i][col]=='Q'){
-            return false;
-        }
-    }
-    for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-        if(board[i][j]=='Q'){
-            return false;
-        }
-    }
-    for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-        if(board[i][j]=='Q'){
-            return false;
-        }
-    }
-    return true;
-}
-void solve(int n,vector<string>&board,vector<vector<string>>&ans,int row,int col){
+void solve(int n,vector<string>&board,vector<vector<string>>&ans,int row){
     //base cses
     if(row==n){
         ans.push_back(board);
@@ -28,20 +13,35 @@ void solve(int n,vector<string>&board,vector<vector<string>>&ans,int row,int col
     }
 
     for(int col=0;col<n;col++){
-        if(isvalid(n,board,row,col)){
-            board[row][col]='Q';
-            solve(n,board,ans,row+1,col);
-            board[row][col]='.';
-           
+        int diagonalconst=row+col;
+        int antidiagonalconst=row-col;
+
+        if(cols.find(col)!=cols.end() || diagonals.find(diagonalconst)!=diagonals.end() || antidiagonals.find(antidiagonalconst)!=antidiagonals.end()){
+            continue;
         }
+
+        board[row][col]='Q';
+        cols.insert(col);
+        diagonals.insert(diagonalconst);
+        antidiagonals.insert(antidiagonalconst);
+
+
+
+        solve(n,board,ans,row+1);
+
+        board[row][col]='.';
+        cols.erase(col);
+        diagonals.erase(diagonalconst);
+        antidiagonals.erase(antidiagonalconst);
+
     }
 }
     vector<vector<string>> solveNQueens(int n) {
         vector<string>board(n,string(n,'.'));
 
         vector<vector<string>>ans;
-
-        solve(n,board,ans,0,0);
+ 
+        solve(n,board,ans,0);
 
         return ans;
     }
