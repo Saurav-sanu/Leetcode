ababd62a384e2f3db1a8ac59dd35454f6909b432
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-05-25 13:16:58 +0530

    Update 0022-generate-parentheses.cpp

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -45,3 +45,45 @@ void solve(int n,vector<string>&ans,string result){
 
     }
 };
+
+
+
+//appraoch 2 withour valid function use 
+class Solution {
+public:
+//approach 2
+void solve(int n,vector<string>&ans,string result,int open,int close){
+    //baes case
+    if(result.size()==2*n){
+       
+            ans.push_back(result);
+
+        return;
+    }
+
+    if(open<n){
+
+        result.push_back('(');
+        solve(n,ans,result,open+1,close);
+        result.pop_back();
+    }
+
+
+    if(close<open){
+
+        result.push_back(')');
+        solve(n,ans,result,open,close+1);
+        result.pop_back();
+    }
+
+
+}
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string result="";
+        int open,close=0;
+        solve(n,ans,result,0,0);
+        return ans;
+
+    }
+};
