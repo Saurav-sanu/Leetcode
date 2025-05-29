f001c213ac631d9681d887174e0ec66b0ca009b5
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-05-29 17:58:52 +0530

    Time: 0 ms (100%), Space: 9.6 MB (18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
 vector<string>result;
-void solve(string digits,int index,unordered_map<char,string>&mp,string temp){
-    //base case
-    if(index>=digits.length()){
-        result.push_back(temp);
-        return;
-    }
 
-    char ch=digits[index];
-    string str=mp[ch];
+    void solve(unordered_map<char,string>&mp,string digits,int idx,string &temp){
+        //base case
+        if(idx==digits.length()){
+            result.push_back(temp);
+            return;
+        }
 
-    for(int i=0;i<str.length();i++){
-        temp.push_back(str[i]);
-        solve(digits,index+1,mp,temp);
-        temp.pop_back();
+        char ch=digits[idx];
+        string str=mp[ch];
+
+        for(int i=0;i<str.length();i++){
+            temp.push_back(str[i]);
+            solve(mp,digits,idx+1,temp);
+            temp.pop_back();
+        }
     }
-}
     vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
         unordered_map<char,string>mp;
         mp['2']="abc";
         mp['3']="def";
@@ -29,12 +31,9 @@ void solve(string digits,int index,unordered_map<char,string>&mp,string temp){
         mp['9']="wxyz";
 
         string temp="";
-        solve(digits,0,mp,temp);
 
-        if(digits.length()==0){
-            return {};
-        }
+        solve(mp,digits,0,temp);
+
         return result;
     }
-    
 };
