d064fb49dfe51bb7e9a9c01e1f54b8c3e716968b
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-05-29 21:23:42 +0530

    Update 0377-combination-sum-iv.cpp

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -24,3 +24,37 @@ int solve(vector<int>&nums,int target,int idx){
 
     }
 };
+
+
+
+//other way of writng the same code
+class Solution {
+public:
+int t[201][1001];
+int solve(vector<int>&nums,int target,int idx){
+    if(target==0){
+        return 1;
+
+    }
+    if(idx>=nums.size() || target<0){
+        return 0;
+    }
+    if(t[idx][target]!=-1){
+        return t[idx][target];
+    }
+    int result=0;
+    for(int i=idx;i<nums.size();i++){
+        int take_i=solve(nums,target-nums[i],0);
+        result +=take_i;
+    }
+
+   
+    return t[idx][target]=result;
+}
+
+    int combinationSum4(vector<int>& nums, int target) {
+        memset(t,-1,sizeof(t));
+        return solve(nums,target,0);
+
+    }
+};
