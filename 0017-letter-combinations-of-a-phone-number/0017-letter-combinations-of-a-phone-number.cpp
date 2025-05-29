d9937fb9fa2c2a3387cb1ce44da639f6d4f44fa1
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-05-29 17:58:36 +0530

    Time: 2 ms (15.78%), Space: 8.9 MB (98.83%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+vector<string>result;
+void solve(string digits,int index,unordered_map<char,string>&mp,string temp){
+    //base case
+    if(index>=digits.length()){
+        result.push_back(temp);
+        return;
+    }
+
+    char ch=digits[index];
+    string str=mp[ch];
+
+    for(int i=0;i<str.length();i++){
+        temp.push_back(str[i]);
+        solve(digits,index+1,mp,temp);
+        temp.pop_back();
+    }
+}
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char,string>mp;
+        mp['2']="abc";
+        mp['3']="def";
+        mp['4']="ghi";
+        mp['5']="jkl";
+        mp['6']="mno";
+        mp['7']="pqrs";
+        mp['8']="tuv";
+        mp['9']="wxyz";
+
+        string temp="";
+        solve(digits,0,mp,temp);
+
+        if(digits.length()==0){
+            return {};
+        }
+        return result;
+    }
+    
+};
