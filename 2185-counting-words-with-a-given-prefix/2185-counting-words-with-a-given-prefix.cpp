3be01dda7df80359467185f8ac34d33bb4618e9e
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-06-04 05:12:43 +0530

    Time: 23 ms (9.55%), Space: 63.5 MB (6.73%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,12 +1,64 @@
 class Solution {
 public:
-    int prefixCount(vector<string>& words, string pref) {
-        int count = 0;
-    for (const string& word : words) {
-        if (word.find(pref) == 0) { 
-            count++;
-        }
+struct trieNode{
+    trieNode* children[26];
+    bool isendofWord;
+    int count;
+};
+
+
+class Trie{
+    public:
+    trieNode* root;
+    Trie(){
+        root=getNode();
     }
-    return count;
+
+    void insert(string &word){
+        trieNode* crawler=root;
+        for(int i=0;i<word.length();i++){
+            int idx=word[i]-'a';
+            if(crawler->children[idx]==NULL){
+                crawler->children[idx]=getNode();
+
+            }
+         
+            crawler=crawler->children[idx];
+            crawler->count++;
+        }
+        crawler->isendofWord=true;
+    }
+    int  search(string &prefix){
+        trieNode* crawler=root;
+        for(int i=0;i<prefix.length();i++){
+            int idx=prefix[i]-'a';
+            if(crawler->children[idx]==NULL){
+                return 0;
+
+            }
+            crawler=crawler->children[idx];
+        }
+        return crawler->count;
+    }
+    private:
+        trieNode* getNode(){
+            trieNode* node=new trieNode();
+            for(int i=0;i<26;i++){
+                node->children[i]=NULL;
+            }
+            node->isendofWord=false;
+            node->count=0;
+
+            return node;
+        }
+};
+    int prefixCount(vector<string>& words, string pref) {
+        Trie trie;
+
+        for(string & word:words){
+            trie.insert(word);
+        }
+
+        return trie.search(pref);
     }
 };
