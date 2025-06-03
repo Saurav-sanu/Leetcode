958113dd185b94b6d2f24eeb64df5847e5b363b1
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-06-04 05:13:57 +0530

    Update 2185-counting-words-with-a-given-prefix.cpp

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,3 +1,20 @@
+// appracoh 1
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0;
+    for (const string& word : words) {
+        if (word.find(pref) == 0) { 
+            count++;
+        }
+    }
+    return count;
+    }
+};
+
+
+// appraoch 2 using trie
+
 class Solution {
 public:
 struct trieNode{
