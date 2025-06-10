5aaf45771fe830c7c1a32cc13b730ef4ad68161f
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-06-10 07:50:21 +0530

    Update 0191-number-of-1-bits.cpp

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,11 +2,41 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int count=0;
+        for(int i=0;i<32;i++){
+            if((1&(n>>i))==1){
+                count++;
+            }
 
-        //approcah 2
+        }
+        return count;
+    }
+};
+
+//approach 2
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
         while(n>0){
             n=n&n-1;
             count++;
+        }
+        return count;
+    }
+};
+
+
+//apprach 3
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+
+        //approcah 3
+        while(n!=0){
+            count+=n%2;
+            n=n/2;
         }
         return count;
     }
