331fab1a13a34198108d15dc336920dd1cf85d5a
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-06-15 03:24:39 +0530

    Update 0201-bitwise-and-of-numbers-range.cpp

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+//approach 1
+    int rangeBitwiseAnd(int left, int right) {
+        int shiftcount=0;
+        while(left!=right){
+            left=(left>>1);
+            right=(right>>1);
+            shiftcount++;
+        }
+        return (left<<shiftcount);
+    }
+};
+
+
+
+class Solution {
+public:
     int rangeBitwiseAnd(int left, int right) {
         //approach 2
 
