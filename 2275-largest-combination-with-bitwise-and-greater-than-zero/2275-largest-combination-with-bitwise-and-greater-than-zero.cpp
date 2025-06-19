2f25d1947ddd6705b26169e7293fb178d13996cf
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-06-19 10:01:20 +0530

    Time: 38 ms (38.52%), Space: 61.4 MB (17.46%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -3,7 +3,10 @@ public:
     int largestCombination(vector<int>& candidates) {
         int result=0;
 
-        for(int bit=0;bit<32;bit++){
+        //constraints dekkhke log (base 2 1o raise paowerr 7 )+1 aaaya 24 ke brabar so loop 24 tak hi chalya hai 
+        
+
+        for(int bit=0;bit<24;bit++){
             int count=0;
             for(int &num:candidates){
                 if(num&(1<<bit)){
