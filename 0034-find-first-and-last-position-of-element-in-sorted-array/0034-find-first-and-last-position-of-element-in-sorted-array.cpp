ef1a052e918d1132244f1900dd8665c50f330846
Saurav <sauravsanu100@gmail.com>
2025-08-04 03:53:11 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.25%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,52 +1,53 @@
 class Solution {
 public:
-int leftmost(vector<int>&nums,int target){
+int leftmost(vector<int>& nums, int target){
     int ans=-1;
-     int start=0;
-        int end=nums.size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-        
-
-            if(nums[mid]==target){
-                ans=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
+    int start=0;
+    int end=nums.size()-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target){
+            ans=mid;
+            end=mid-1;
+            
         }
-        return ans;
+        else if(nums[mid]<target){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return ans;
 }
-int rightmost(vector<int>&nums,int target){
+int rightmost(vector<int>& nums, int target){
     int ans=-1;
-     int start=0;
-        int end=nums.size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-
-            if(nums[mid]==target){
-                ans=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
+    int start=0;
+    int end=nums.size()-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target){
+            ans=mid;
+            start=mid+1;
+            
         }
-        return ans;
+        else if(nums[mid]<target){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return ans;
 }
     vector<int> searchRange(vector<int>& nums, int target) {
+        int left=leftmost(nums,target);
+        int right=rightmost(nums,target);
         vector<int>ans;
-       int left=leftmost(nums,target);
-       int right=rightmost(nums,target);
-       ans.push_back(left);
-       ans.push_back(right);
-       return ans;
+        ans.push_back(left);
+        ans.push_back(right);
+        
+        return ans;
+        
     }
 };
