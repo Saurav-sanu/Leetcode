712a81595673c9bcb24a12be1b33f1bd9516ed68
Saurav <sauravsanu100@gmail.com>
2025-08-11 18:01:58 +0530

    Time: 7 ms (36.41%), Space: 26 MB (7.66%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,68 +1,26 @@
 class Solution {
 public:
-    bool valid(int sr,int er,int sc,int ec,vector<vector<char>>& board){
-        unordered_set<char>st;
-        for(int i=sr;i<=er;i++){
-            for(int j=sc;j<=ec;j++){
-                if(board[i][j]=='.'){
-                    continue;
-                }
-                if(st.find(board[i][j])!=st.end()){
-                    return false;
-                }
-
-                st.insert(board[i][j]);
-            }
-        }
-        return true;
-    }
     bool isValidSudoku(vector<vector<char>>& board) {
-        int m=board.size();
-        int n=board[0].size();
-
-        //validate row;
-        for(int row=0;row<m;row++){
-            unordered_set<char>st;
-            for(int col=0;col<n;col++){
-                if(board[row][col]=='.'){
+        
+        unordered_set<string>st;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+               if(board[i][j]=='.'){
                     continue;
-                }
+               }
+               string row=string(1,board[i][j])+"ROW"+to_string(i);
+               string col=string(1,board[i][j])+"COL"+to_string(j);
+               string box=string(1,board[i][j])+"ROWcol"+to_string(i/3)+ "_" +to_string(j/3);
 
-                if(st.find(board[row][col])!=st.end()){
+                if(st.find(row)!=st.end() || st.find(col)!=st.end() ||st.find(box)!=st.end()){
                     return false;
                 }
 
-                st.insert(board[row][col]);
-            }
-        } 
-        //validate col
-        for(int col=0;col<n;col++){
-            unordered_set<char>st;
-            for(int row=0;row<m;row++){
-                if(board[row][col]=='.'){
-                    continue;
-                }
-
-                if(st.find(board[row][col])!=st.end()){
-                    return false;
-                }
-
-                st.insert(board[row][col]);
-            }
-        } 
-
-        for(int sr=0;sr<9;sr+=3){
-            int endr=sr+2;
-            for(int sc=0;sc<9;sc+=3){
-                int endc=sc+2;
-                if(!valid(sr,endr,sc,endc,board)){
-                    return false;
-                }
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
-
         return true;
-        
     }
-
 };
