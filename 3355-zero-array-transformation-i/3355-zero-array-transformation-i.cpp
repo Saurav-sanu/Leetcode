32cf64f5c9284f0c62d741a1da6f3e6001d3cb90
Saurav <sauravsanu100@gmail.com>
2025-08-13 04:32:16 +0530

    Time: 128 ms (30.88%), Space: 327.6 MB (6.36%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        vector<int>diff(n,0);
+        for(auto q:queries){
+            int s=q[0];
+            int e=q[1];
+            int x=1;
+
+            diff[s]+=x;
+
+
+            if(e+1<n){
+                diff[e+1]-=x;
+            }
+        }
+        vector<int>cumalative(n,0);
+        cumalative[0]=diff[0];
+        for(int i=1;i<n;i++){
+            cumalative[i]=diff[i]+cumalative[i-1];
+        }
+
+        for(int i=0;i<n;i++){
+            if(cumalative[i]<nums[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
