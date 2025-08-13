fb9ec978e5c92da60985db314713e33ddc16d2be
Saurav <sauravsanu100@gmail.com>
2025-08-13 18:12:25 +0530

    Time: 0 ms (100%), Space: 10.8 MB (95.91%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string ans="";
+        for(int i=0;i<num.size();i++){
+            while(ans.length() >0 && ans.back()>num[i] && k>0){
+                ans.pop_back();
+                k--;
+            }
+
+            if(num[i]!='0' || ans.length()>0){
+                ans.push_back(num[i]);
+            }
+        }
+
+        //12345 k=3
+        while(ans.length()>0 && k--){
+            ans.pop_back();
+
+        }
+        if(ans==""){
+            return "0";
+        }
+
+        return ans;
+
+    }
+};
