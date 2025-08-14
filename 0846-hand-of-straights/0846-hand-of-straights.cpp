e60eacf04698d81acc0c87fbace3b12971c6eca9
Saurav <sauravsanu100@gmail.com>
2025-08-14 17:16:09 +0530

    Time: 47 ms (22.69%), Space: 32.4 MB (38.85%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -5,27 +5,26 @@ public:
         if(n%groupSize!=0){
             return false;
         }
-
         map<int,int>mp;
-        for(int &h:hand){
-            mp[h]++;
+        for(auto &ha:hand){
+            mp[ha]++;
         }
-        while(!mp.empty()){
-            //first eleemnt is number 
-            //second is frequency
-            int curr=mp.begin()->first;
 
+        while(!mp.empty()){
+            int curr=mp.begin()->first;
             for(int i=0;i<groupSize;i++){
                 if(mp[curr+i]==0){
                     return false;
                 }
+                
                 mp[curr+i]--;
                 if(mp[curr+i]<1){
                     mp.erase(curr+i);
                 }
+                
             }
         }
         return true;
-      
+
     }
 };
