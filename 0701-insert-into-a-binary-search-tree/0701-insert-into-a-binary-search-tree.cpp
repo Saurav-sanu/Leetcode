7baa06db4cae52054e28e22b0dfe8276786ff13f
Saurav <sauravsanu100@gmail.com>
2025-08-24 03:51:10 +0530

    Update 0701-insert-into-a-binary-search-tree.cpp

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -28,3 +28,58 @@ public:
 
 
 };
+
+
+
+//iterative approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            root=new TreeNode(val);
+            return root;
+        }
+
+        TreeNode* newNode=new TreeNode(val);
+        TreeNode* curr=root;
+
+        while(root){
+            if(root->val>val){
+                //left mai jao
+                if(root->left==NULL){
+                    root->left=newNode;
+                    break;
+                }
+                else{
+
+                root=root->left;
+                }
+
+
+            }
+            else{
+                if(root->right==NULL){
+                    root->right=newNode;
+                    break;
+                }
+
+                else{
+
+                root=root->right;
+                }
+            }
+        }
+        return curr;
+    }
+};
