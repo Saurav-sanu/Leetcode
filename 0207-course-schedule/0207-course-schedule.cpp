d6256a3eb389242e03efebd9666c796af1c0a810
Saurav <sauravsanu100@gmail.com>
2025-08-27 02:45:30 +0530

    Time: 6 ms (54.25%), Space: 20.2 MB (31.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,55 +1,48 @@
 class Solution {
 public:
-bool toposort(unordered_map<int,vector<int>>&adj,vector<int>&indegree,int n){
-    //step 1
-    int count=0;
-
-    queue<int>q;
-    for(int i=0;i<n;i++){
-        if(indegree[i]==0){
-            q.push(i);
-        }
-    }
-
-    while(!q.empty()){
-        int top=q.front();
-        count++;
-        q.pop();
-
-        for(int &v:adj[top]){
-            indegree[v]--;
-            if(indegree[v]==0){
-                q.push(v);
-            }
-
-        }
-
-    }
-
-    if(count==n){
-        return true;
-    }
-    else{
-        return false;
-    }
-
-}
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        //agar cycle hua toh fasle other wise not false;
-        unordered_map<int,vector<int>>adj;
+        //if cycle so false
+        int count=0;
+        int n=numCourses;
         vector<int>indegree(numCourses,0);
-
+        unordered_map<int,vector<int>>adj;
         for(auto &vec:prerequisites){
             int u=vec[0];
             int v=vec[1];
 
             adj[v].push_back(u);
-
             indegree[u]++;
         }
 
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0){
+                q.push(i);
+                count++;
+            }
+        }
 
-        return toposort(adj,indegree,numCourses);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
 
+            for(auto &v:adj[node]){
+
+                indegree[v]--;
+
+                if(indegree[v]==0){
+                    q.push(v);
+                    count++;
+                }
+            }
+        }
+
+        if(count==n){
+            return true;
+        }
+        return false;
+
+
+        
     }
 };
