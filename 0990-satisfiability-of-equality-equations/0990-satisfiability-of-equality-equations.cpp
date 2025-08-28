f069b86bd0330440fad2d429ef62e7b1c064fa0d
Saurav <sauravsanu100@gmail.com>
2025-08-29 05:28:43 +0530

    Time: 0 ms (100%), Space: 14.8 MB (93.85%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -1,63 +1,62 @@
 class Solution {
 public:
-vector<int>rankA;
 vector<int>parent;
-
+vector<int>rank;
 int find(int x){
     if(x==parent[x]){
         return x;
     }
-
     return parent[x]=find(parent[x]);
 }
-
-void Union(int x,int y){
+void Union(int x ,int y){
+    //base case
     int x_parent=find(x);
     int y_parent=find(y);
-
     if(x_parent==y_parent){
         return;
     }
 
-    if(rankA[x_parent]<rankA[y_parent]){
+    if(rank[x_parent]>rank[y_parent]){
+        parent[y_parent]=x_parent;
+       
+    }
+    else if(rank[y_parent]>rank[x_parent]){
         parent[x_parent]=y_parent;
     }
-    else if(rankA[y_parent]<rankA[x_parent]){
-        
-        parent[y_parent]=x_parent;
-    }
     else{
-        parent[y_parent]=x_parent;
-        rankA[x_parent]+=1;
+        parent[x_parent]=y_parent;
+        rank[y_parent]++;
 
     }
 
 }
-
     bool equationsPossible(vector<string>& equations) {
-        rankA.resize(26,0);
+        int n=equations.size();
         parent.resize(26);
-
+        rank.resize(26);
         for(int i=0;i<26;i++){
             parent[i]=i;
+            rank[i]=1;
         }
 
-        for(auto &s:equations){
+        for(string &s:equations){
             if(s[1]=='='){
                 Union(s[0]-'a',s[3]-'a');
             }
         }
-        for(auto &s:equations){
+        for(string &s:equations){
             if(s[1]=='!'){
-                int first_parent=find(s[0]-'a');
-                int second_parent=find(s[3]-'a');
+                int x_parent=find(s[0]-'a');
+                int y_parent=find(s[3]-'a');
 
-                if(first_parent==second_parent){
+                if(x_parent==y_parent){
                     return false;
+                    
                 }
             }
-            
+
         }
+        
         return true;
     }
 };
