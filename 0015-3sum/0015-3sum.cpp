82e9783b660ec6a570b834425a50f7d8c7e878cb
Saurav <sauravsanu100@gmail.com>
2025-10-17 21:21:47 +0530

    Time: 49 ms (56.85%), Space: 33.2 MB (17.74%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        int n=arr.size();
-        vector<vector<int>>result;
-        sort(arr.begin(),arr.end());
+vector<vector<int>>result;
+    void twosum(int target,vector<int>&nums,int s,int e){
+        int i=s;
+        int j=e;
+
+        while(i<j){
+            if(nums[i]+nums[j]>target){
+                j--;
+            }
+            else if(nums[i]+nums[j]<target){
+                i++;
+            }
+            else{
+
+                while(i<j&& nums[i]==nums[i+1]){
+                    i++;
+                }
+                while(i<j && nums[j]==nums[j-1]){
+                    j--;
+                }
+                result.push_back({-target,nums[i],nums[j]});
+
+                i++;
+                j--;
+            }
+           
+
+        }
+
+
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        if(n<3){
+            return {};
+        }
+        sort(nums.begin(),nums.end());
+        result.clear();
+
         for(int i=0;i<n;i++){
-            if(i!=0 && arr[i]==arr[i-1]){
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            int j=i+1;
-            int k=n-1;
-            while(j<k){
+            int target=nums[i];
 
-                if((arr[i]+arr[j]+arr[k])<0){
-                    j++;
-                    
-                }
-                else if((arr[i]+arr[j]+arr[k])>0){
-                    k--;
-
-                }
-                else{
-                    vector<int>temp={arr[i],arr[j],arr[k]};
-                    
-                    result.push_back(temp);
-                    j++;
-                    k--;
-                    while(j<k && arr[j]==arr[j-1])j++;
-                    while(j<k && arr[k]==arr[k+1])k--;
-                }
-            }
+            twosum(-target,nums,i+1,n-1);
 
         }
         return result;
-
     }
 };
