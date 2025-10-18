ac3a1faadf6c564789bd54fc3a50672c62be1f1d
Saurav <sauravsanu100@gmail.com>
2025-10-19 01:10:14 +0530

    Time: 0 ms (100%), Space: 15.1 MB (65.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start=0;
-        int end=nums.size()-1;
-        int ans=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                
-                return mid;
-            }
-            //left mai search karo
-            if(nums[start]<=nums[mid]){
-                if(nums[start]<=target && target<=nums[mid]){
-                    end=mid-1;
-                }
-                else{
-                    start=mid+1;
-                }
-                
-            }
-            else{
-                 if(nums[mid]<=target && target<=nums[end]){
-                    start=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
+int binary(vector<int>&nums,int target,int start,int end){
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
+        else if(nums[mid]<target){
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+int min(vector<int>&nums,int n){
+    int l=0;
+    int r=n-1;
+    while(l<r){
+        int mid=l+(r-l)/2;
+        if(nums[mid]>nums[r]){
+            l=mid+1;
+
+        }
+        else{
+            r=mid;
+        }
+    }
+    return r;
+}
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int pivot=min(nums,n);
+        int one=binary(nums,target,0,pivot-1);
+        if(one!=-1){
+            return one;
+        }
+        int two=binary(nums,target,pivot,n-1);
+        return two;
     }
 };
-
