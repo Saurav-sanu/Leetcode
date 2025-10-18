f1de6d8f82e81e5f019e748480a6a5e3b44c22cb
Saurav <sauravsanu100@gmail.com>
2025-10-18 15:46:00 +0530

    Time: 3 ms (89.62%), Space: 23.7 MB (94.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        int i = 0;
-        int j = 1;
-        int n = intervals.size();
-        vector<vector<int>> result;
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+        int n=in.size();
+        sort(in.begin(),in.end());
+        vector<vector<int>>ans;
 
-        while (j < n) {
-            int curr_start = intervals[i][0];
-            int curr_end = intervals[i][1];
+        int i=0;
+        int j=i+1;
+        while(j<n){
+            int cs=in[i][0];
+            int ce=in[i][1];
 
-            int next_start = intervals[j][0];
-            int next_end = intervals[j][1];
+            int ns=in[j][0];
+            int ne=in[j][1];
 
-            // Overlapping condition
-            if (curr_end >= next_start) {
-                // Merge intervals
-                curr_end = max(curr_end, next_end);
-                intervals[i] = {curr_start, curr_end};
-            } else {
-                // Push merged interval and move to next
-                result.push_back({curr_start, curr_end});
-                i = j;
+            //overlapping condition
+            if(ce>=ns){
+                
+                ce=max(ne,ce);
+                in[i]={cs,ce};
+            }
+            else{
+                ans.push_back({cs,ce});
+                i=j;
+                
             }
             j++;
-        }
-        
-        // Push the last interval
-        result.push_back(intervals[i]);
 
-        return result;
+        }
+
+        ans.push_back(in[i]);
+        return ans;
+
     }
 };
