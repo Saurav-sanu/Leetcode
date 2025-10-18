680e65b9fe5abcbd0433e724d21fe4ec96d310a6
Saurav <sauravsanu100@gmail.com>
2025-10-18 16:01:17 +0530

    Time: 0 ms (100%), Space: 12.3 MB (70.75%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
+    void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0&& j>=0){
+            if(arr1[i]>arr2[j]){
+                arr1[k--]=arr1[i--];
+            }
+            else{
+                arr1[k--]=arr2[j--];
+            }
 
-    void merge(vector<int>& arr1, int n, vector<int>& arr2, int m) {
-        vector<int>merged;
-        int i=0;
-      int j=0;
-      while(i<n && j<m){
-          if(arr1[i]<arr2[j]){
-              merged.push_back(arr1[i]);
-              i++;
-          }
-          else{
-              merged.push_back(arr2[j]);
-              j++;
-          }
-      }
-      while(i<n){
-          merged.push_back(arr1[i]);
-          i++;
-      }
-      while(j<m){
-          merged.push_back(arr2[j]);
-          j++;
-      }
+        }
 
-      for (int k = 0; k < n + m; ++k) {
-            arr1[k] = merged[k];
+        while(j>=0){
+            arr1[k--]=arr2[j--];
         }
     }
 };
