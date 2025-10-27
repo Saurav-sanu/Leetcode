fa93dd4420d50359fa8a3de4e2d83513b27bdb99
Saurav <sauravsanu100@gmail.com>
2025-10-27 18:12:20 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.62%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>smap;
-        unordered_map<char,char>tmap;
-
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
         for(int i=0;i<s.length();i++){
-            char sk=s[i];
-            char tk=t[i];
-
-            if(smap.find(sk)!=smap.end() && smap[sk]!=tk || tmap.find(tk)!=tmap.end() && tmap[tk]!=sk){
+            if(mp1.find(s[i])!=mp1.end() && mp1[s[i]]!=t[i] || mp2.find(t[i])!=mp2.end() && mp2[t[i]]!=s[i]){
                 return false;
             }
-            smap[sk]=tk;
-            tmap[tk]=sk;
+            else{
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+
+            }
         }
         return true;
-
-        
     }
 };
-
-
-
-//  bool isIsomorphic(string s, string t) {
-//         int hash[300]={0};
-//         bool isTCharMapped[300]={0};
-
-//         for(int i=0;i<s.size();i++){
-//             if(hash[s[i]]==0 && isTCharMapped[t[i]]==0){
-//                 hash[s[i]]=t[i];
-//                 isTCharMapped[t[i]]=true;
-//             }
-//         }
-//         for(int i=0;i<s.length();i++){
-//             if(char(hash[s[i]])!=t[i]){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
