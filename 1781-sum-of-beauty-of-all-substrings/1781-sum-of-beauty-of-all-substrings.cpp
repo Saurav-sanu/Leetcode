b49a8d0a31334023d8b7dc27363c5fb82ce488f1
Saurav <sauravsanu100@gmail.com>
2025-10-29 01:31:18 +0530

    Time: 123 ms (35.42%), Space: 25.9 MB (34.67%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-int mini(vector<int>&str){
-    int minElement=INT_MAX;
-    for(int i=0;i<str.size();i++){
-        if(str[i]!=0){
-            minElement=min(minElement,str[i]);
-        }
-    }
-    return minElement;
-}
-int maxi(vector<int>&str){
-    return *max_element(str.begin(),str.end());
-
-}
     int beautySum(string s) {
-        int sum=0;
-        int n=s.length();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            unordered_map<char,int>mp;
+            for(int j=i;j<s.length();j++){
+                int mini=INT_MAX;
+                int maxi=INT_MIN;
+                mp[s[j]]++;
+               for(auto &p:mp){
+                mini=min(mini,p.second);
+                maxi=max(maxi,p.second);
+               }
 
-        for(int i=0;i<n;i++){
-            vector<int>str(256,0);
-            for(int j=i;j<n;j++){
-                str[s[j]-'a']++;
-                int diff=maxi(str)-mini(str);
-                sum+=diff;
+                count+=maxi-mini;
+
             }
-            
+
         }
-        return sum;
+        return count;
     }
 };
