6245777f0c21898658ae7d3c2e50891d4dc2f911
Saurav <sauravsanu100@gmail.com>
2025-11-01 22:53:57 +0530

    Time: 3 ms (99.03%), Space: 11.9 MB (52.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -14,13 +14,19 @@ public:
         if(head==NULL || head->next==NULL){
             return false;
         }
+
         while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast){
                 return true;
             }
+            
+
         }
+
         return false;
+
+        
     }
 };
